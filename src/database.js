2125d25e7c52583b226fb7106dcb661eec2b760c
daniel.barbosa <daniel.barbosa@cuprum.com>
2022-03-11 21:36:02 -0600

    db

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-// mongoose.connect('mongodb+srv://admin:Baumer8945.@eqm.pcblw.mongodb.net/CRMQA?authSource=admin&replicaSet=atlas-h6ch0g-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(db => console.log('Db is connected'))
-//     .catch(err => (console.error(err)))
-mongoose.connect('mongodb://localhost:27017/CRM?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
+mongoose.connect('mongodb+srv://admin:Baumer8945.@eqm.pcblw.mongodb.net/CRMQA?authSource=admin&replicaSet=atlas-h6ch0g-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => console.log('Db is connected'))
     .catch(err => (console.error(err)))
+// mongoose.connect('mongodb://localhost:27017/CRM?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(db => console.log('Db is connected'))
+//     .catch(err => (console.error(err)))
